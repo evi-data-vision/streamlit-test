17b0eb53b62181bbdecb74e32d272869749a0b22
evi-data-vision <ehsan_7@ymail.com>
2025-07-13 18:05:27 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title('Title!')
+
+a = st.radio('select ' , ['a' , 'b' , 'c'] , index  = None)
+
+b = st.slider('Select the length : ' , min_value = 6 , max_value = 32 , value = 8)
+
+upper = st.toggle('d')
+digits = st.toggle('e')
+sign = st.toggle('f')
+
+st.write('HI!!!!!!!!!!!1')
+
+st.selectbox('Seperator : ' , ['-' , '_' , ',' , '.'])
+
+st.write('Your Generated Memorable Password is : ' a + str(b) + ' ' + str(upper) + ' ' + str(digits) + ' ' + str(sign))
+
